bccc83617bcca7f4c930012c527b75c57ad4b227
Charles Lee <charlesjhlee@gmail.com>
2020-11-13 00:14:29 +1100

    updated shield settings

--- implement/applications/eve-online/eve-online-mining-bot/BotEngineApp.elm
+++ implement/applications/eve-online/eve-online-mining-bot/BotEngineApp.elm
@@ -88,7 +88,7 @@ import Regex
 -}
 defaultBotSettings : BotSettings
 defaultBotSettings =
-    { runAwayShieldHitpointsThresholdPercent = 0
+    { runAwayShieldHitpointsThresholdPercent = 10
     , unloadStationName = Nothing
     , unloadStructureName = Nothing
     , modulesToActivateAlways = []
